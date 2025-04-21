96646373a6c5cbe1ef7291d8012ee617b4e80c89
UlayyaAqila <ulayyaqilah12@gmail.com>
2025-04-21 19:58:33 +0800

    Add files via upload

---
+++ tugas 1.py
@@ -0,0 +1,20 @@
+x = True
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = True
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
